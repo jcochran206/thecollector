71f8ded8b783fef02c467c74a40d85e9fdb59764
jcochran206 <jonathan.o.cochran@gmail.com>
2022-08-06 13:38:40 -0700

    created game model

--- server/models/game.model.js
+++ server/models/game.model.js
@@ -1,8 +1,36 @@
 const mongoose = require('mongoose');
 const GameSchema = new mongoose.Schema({
-    
+  title: {
+    type: String,
+    required: [true, 'game title is required'],
+    maxlength: [40, 'length max is 40 chars']
+  },
+  rating: {
+    type: String,
+    required: [true, 'game rating is needed'],
+    enum: ['E', 'E10', 'T', 'M', 'AO' ]
+  },
+  platform: {
+    type: String,
+    required: [true, 'platform is needed'],
+  },
+  edition: {
+    type: String,
+  },
+  price: {
+    type: String,
+    default: 'N/A'
+  },
+  manufacturer: {
+    type: String,
+  },
+  releaseYear: {
+    type: Number,
+    min: [1960, 'too old']
+  }
+
 }, {timestamps: true})
 
-const Game = mongoose.model('Game', GameSchema);
+const Game = mongoose.model('game', GameSchema);
 
 module.exports = Game;
