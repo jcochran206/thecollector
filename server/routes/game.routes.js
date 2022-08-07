70c7264bbde7fc292db3903eaaa74b715dc88290
jcochran206 <jonathan.o.cochran@gmail.com>
2022-08-07 10:51:26 -0700

    added routes for server

---
+++ server/routes/game.routes.js
@@ -0,0 +1,9 @@
+const gameController = require('../controllers/game.controller');
+
+module.exports = (app) => {
+    app.get('/api/games', gameController.findAllGames);
+    app.get('/api/games/:id', gameController.findOneGame);
+    app.post('/api/games/:id', gameController.createOneGame);
+    app.put('/api/games/:id', gameController.updateAGame);
+    app.delete('/api/games/:id', gameController.deleteOneGame);
+};
