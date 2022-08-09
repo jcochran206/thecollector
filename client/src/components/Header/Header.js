813518dfd4ded89e74330ef551f367e992ed7cb1
jcochran206 <jonathan.o.cochran@gmail.com>
2022-08-08 23:13:00 -0700

    modified header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,27 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {GiConsoleController} from 'react-icons/gi'
+import {FaBars} from 'react-icons/fa'
 import {NavLink} from 'react-router-dom'
 
+
 const Header = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <header>
-    <div className='shadow-md w-full top-0 left-0'>
-        <div className='md:flex bg-white py-4 md:px-10 px-7'>
-        <div className='font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] text-gray-800'>
-            <span className='text-3x1 text-indigo-600 mr-1 pt-2'>
-                <GiConsoleController />
-            </span>
-            collector
+    <>
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-slate-500 mb-3">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <NavLink
+            className="text-sm font-bold leading-relaxed flex mr-4 py-3 whitespace-nowrap uppercase text-white"
+            to="/"
+          >
+            Collectors <GiConsoleController/> Games
+          </NavLink>
+          <button
+            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <FaBars />
+          </button>
         </div>
-        <NavLink to='/'>
-            Home
-        </NavLink>
-        <NavLink to='/new'>
-            Add New Game
-        </NavLink>
+        <div
+          className={
+            "lg:flex flex-grow items-center" +
+            (navbarOpen ? " flex" : " hidden")
+          }
+          id="example-navbar-danger"
+        >
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <NavLink className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to='/'>
+                Home
+            </NavLink>
+            <NavLink className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to='/new'>
+                Add New Game
+            </NavLink>
+          </ul>
         </div>
-    </div>
-    </header>
+      </div>
+    </nav>
+  </>
+
   )
 }
 
